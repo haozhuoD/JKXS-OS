573c6cadbec008cc1eceb98b09c04726f84d8182
dhz <256769508@qq.com>
2022-05-08 02:02:37 +0800

    test

--- os/src/main.rs
+++ os/src/main.rs
@@ -59,6 +59,7 @@ static AP_CAN_INIT: AtomicBool = AtomicBool::new(false);
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    println!("[kernel] hello this is  rust_main");
     save_hartid();
     let hartid = get_hartid();
     println!("[kernel] Riscv hartid {} init ", hartid);
