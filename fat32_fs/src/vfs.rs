56e112ca6a53418df68eb7dc574c8387340ed8c9
Dh2zZ <2567769508@qq.com>
2022-08-08 05:01:47 +0800

    修复了一个文件名含有两个.跑不了的问题

--- fat32_fs/src/vfs.rs
+++ fat32_fs/src/vfs.rs
@@ -288,9 +288,11 @@ impl VFile {
         assert!(self.is_dir());
         let (name_, ext_) = name.rsplit_once(".").unwrap_or((name, ""));
         self.read_short_dirent(|short_ent| {
-            if name_.len() > 8 || ext_.len() > 3 {
+            if name_.len() > 8 || ext_.len() > 3 || name_.contains(".") {
+                println!("long = {:#x?}", name);
                 return self.find_long_name(name, short_ent);
             } else {
+                println!("short = {:#x?}", name);
                 return self.find_short_name(name, short_ent);
             }
         })
