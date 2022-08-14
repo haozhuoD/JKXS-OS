358f313be06285b8d8905869e6681475d7bb2b56
Dh2zZ <2567769508@qq.com>
2022-08-15 00:14:12 +0800

    O3 optimize

--- os/src/fs/vfile.rs
+++ os/src/fs/vfile.rs
@@ -132,16 +132,14 @@ impl OSFile {
         let mut kstat = Kstat::new();
         let inner = self.inner.lock();
         kstat.st_mode = {
-            if inner.vfile.get_name() == "null" {
-                S_IFCHR
-            } else if inner.vfile.is_dir() {
+            if inner.vfile.is_dir() {
                 S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO
             } else {
                 S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO
             }
         };
-        kstat.st_ino = inner.vfile.first_cluster() as u64;
-        kstat.st_size = inner.vfile.get_size() as i64;
+        // kstat.st_ino = inner.vfile.first_cluster() as u64;
+        // kstat.st_size = inner.vfile.get_size() as i64;
         kstat.st_atime_sec = inner.atime as i64;
         kstat.st_mtime_sec = inner.mtime as i64;
         kstat
